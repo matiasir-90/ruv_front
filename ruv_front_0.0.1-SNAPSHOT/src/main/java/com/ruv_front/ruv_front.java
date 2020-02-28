de95e8a46a4ad78f31b042899205dc9d53dc48e7
matias <matias@172.28.16.208>
2020-02-28 11:51:13 -0300

    funcionando calendario

--- ruv_front_0.0.1-SNAPSHOT/src/main/java/com/ruv_front/ruv_front.java
+++ ruv_front_0.0.1-SNAPSHOT/src/main/java/com/ruv_front/ruv_front.java
@@ -1,9 +1,12 @@
-package com.ruv_front.tsb;
+package com.ruv_front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan("http:\\localhotst:8181")
 public class ruv_front {
 
     public static void main(String[] args) {
--- ruv_front_0.0.1-SNAPSHOT/src/main/java/com/ruv_front/tsb/controller/CalendarioController.java
+++ ruv_front_0.0.1-SNAPSHOT/src/main/java/com/ruv_front/tsb/controller/CalendarioController.java
@@ -1,59 +1,64 @@
 package com.ruv_front.tsb.controller;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.ruv_front.tsb.entity.CalendarioEntity;
-import com.ruv_front.tsb.model.CalendarioModel;
-import com.ruv_front.tsb.service.CalendarioService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
-@CrossOrigin(origins = "http://localhost:8181",maxAge=3600)
+import com.ruv_front.tsb.Model.CalendarioModel;
+
 @RestController
-@RequestMapping({"/calendario"})
+@RequestMapping(value = "/calendario" )
 
 public class CalendarioController {
+	
 
-@Autowired
-CalendarioService service;
-
-@GetMapping("/")
-    public String ViewIndex(){
-       public List<CalendarioModel> getcalendario() 
-    }
-        return "index";
-        
-{
     
-    return service.getCalendario();
-}
-@PostMapping
-public CalendarioEntity agregarC(@RequestBody CalendarioModel p){
-    return service.add(p);
-}
-@GetMapping(path = {"/{id}"})
-public CalendarioEntity listarId(@PathVariable("id")int id){
-    return service.listarId(id);
-}
-@PutMapping(path = {"/{id}"})
-public CalendarioEntity editar(@RequestBody CalendarioModel p,@PathVariable("id") int id){
-    p.setId(id);
-    return service.edit(p);
-}
-@DeleteMapping(path = {"/{id}"})
-public CalendarioEntity delete(@PathVariable("id") int  id){
-    return service.delete(id);
-}
+    @GetMapping
+    public String ViewCalendario(Model model){
+    	
+    	CalendarioModel calendario = new CalendarioModel();
+    	
+         model.addAttribute("calendariolist",calendario);
+         return "calendariolist";
+    }
+    public CalendarioModel getcalendario()
+	{
+	    final String uri = "http://localhost:8181/calendario";
+	    RestTemplate restTemplate = new RestTemplate();
+	     
+	    CalendarioModel result = restTemplate.getForObject(uri, CalendarioModel());	  
+	    System.out.println(result);
+	    return result; 
 
-}
+	}
+
+	@GetMapping("/")
+	public String showUpdateForm(Model model) {
+		model.addAttribute("calendario", getcalendario());
+		return "index";
+	}
+	}
+	
+//    
+//@PostMapping
+//public CalendarioEntity agregarC(@RequestBody CalendarioModel p){
+//    return service.add(p);
+//}
+//@GetMapping(path = {"/{id}"})
+//public CalendarioEntity listarId(@PathVariable("id")int id){
+//    return service.listarId(id);
+//}
+//@PutMapping(path = {"/{id}"})
+//public CalendarioEntity editar(@RequestBody CalendarioModel p,@PathVariable("id") int id){
+//    p.setId(id);
+//    return service.edit(p);
+//}
+//@DeleteMapping(path = {"/{id}"})
+//public CalendarioEntity delete(@PathVariable("id") int  id){
+//    return service.delete(id);
+//}
+//                                                                                            
